5fd0c43c5b5aa8e1decbcbd4c92a021ec2e25860
Marius <65131615+woohoos@users.noreply.github.com>
2025-02-17 14:48:42 +0200

    Test commit

--- scripts/generate_trackDb.R
+++ scripts/generate_trackDb.R
@@ -1,33 +1,56 @@
 github_user <- "woohoos"
 repo_name <- "ucsc-tracks"
-
 base_url <- paste0("https://raw.githubusercontent.com/", github_user, "/", repo_name, "/main/tracks/")
 
+# Extensions
 track_types <- list(
   ".bw" = "bigWig",
   ".bedGraph" = "bedGraph",
   ".bigBed" = "bigBed"
 )
 
-track_files <- list.files("tracks", pattern = paste0("\\", names(track_types), collapse = "|"), full.names = FALSE)
+# Colors
+color_blue <- "0,0,255"         # methylation
+color_red <- "255,0,0"          # pValue/control
+color_green <- "63,127,9"       # acetylation (Hunter Green)
+color_default <- "0,0,0"        
 
+track_dirs <- list.dirs("tracks", recursive = FALSE)  
 track_db <- c()
-for (track in track_files) {
-  ext <- tools::file_ext(track)
-  track_type <- track_types[[paste0(".", ext)]]
-  track_name <- sub(paste0(".", ext, "$"), "", track)
+
+for (folder in track_dirs) {
+  folder_name <- basename(folder)  
+  track_files <- list.files(folder, pattern = paste0("\\", names(track_types), collapse = "|"), full.names = FALSE)
   
-  track_db <- c(track_db,
-                paste0("track ", track_name),
-                paste0("bigDataUrl ", base_url, track),
-                paste0("type ", track_type),
-                paste0("shortLabel ", track_name),
-                paste0("longLabel ", track_name, " data"),
-                "visibility full",
-                ""
-  )
+  for (track in track_files) {
+    ext <- tools::file_ext(track)
+    track_type <- track_types[[paste0(".", ext)]]
+    track_name <- sub(paste0(".", ext, "$"), "", track)
+    track_path <- paste0(folder, "/", track)  
+    
+    if (grepl("methylation", track, ignore.case = TRUE)) {
+      track_color <- color_blue
+    } else if (grepl("pvalue|kontrol|control", track, ignore.case = TRUE)) {
+      track_color <- color_red
+    } else if (grepl("acetylation", track, ignore.case = TRUE)) {
+      track_color <- color_green
+    } else {
+      track_color <- color_default
+    }
+    
+    track_db <- c(track_db,
+                  paste0("# Collection: ", folder_name),
+                  paste0("track ", track_name),
+                  paste0("bigDataUrl ", base_url, track_path),
+                  paste0("type ", track_type),
+                  paste0("shortLabel ", track_name),
+                  paste0("longLabel ", track_name, " (", folder_name, ")"),
+                  paste0("color ", track_color),
+                  "visibility full",
+                  ""
+    )
+  }
 }
 
 writeLines(track_db, "trackDb.txt")
-
-cat("rackDb.txt updated!!!\n")
+cat("✅ trackDb.txt updated with collections and color-coded tracks!\n")
